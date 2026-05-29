2d84ef29e8e76f9d17ea3db239f4edd75ffc07d4
Weshiwes <k3thprop@gmail.com>
2026-05-29 23:24:45 +0800

    add main

---
+++ DataAnalysiswithPython/Page_View_Time_Series_Visualizer/main.py
@@ -0,0 +1,8 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+from unittest import main
+
+draw_line_plot()
+draw_bar_plot()
+draw_box_plot()
+
+main(module='test_module', exit=False)
